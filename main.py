e5b1ec6f060f71327008f90c4483e33202562fa0
Ganesh <ganuofficial2795@gmail.com>
2021-04-29 14:14:15 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+
+# import required modules
+import cv2 as cv
+from random import randrange
+
+
+
+
+
+
+if __name__ == "__main__":
+
+
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+    print("Code Completed 🔥")
