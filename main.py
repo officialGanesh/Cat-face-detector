b0376e55832ef03ac8d72777a5d940af9e042acc
Ganesh <ganuofficial2795@gmail.com>
2021-04-29 14:20:59 +0530

    Detected faces

--- main.py
+++ main.py
@@ -4,12 +4,21 @@ import cv2 as cv
 from random import randrange
 
 
+def Detect_face():
+    """This function is used to detect faces of cat from an image and convert into grayscale"""
 
+    img = cv.imread('cat-face.jpg')
+    cv.imshow("Cat",img)
 
+    gray = cv.cvtColor(img,cv.COLOR_RGB2GRAY)
+    cv.imshow('Gray-cat',gray)
+
+    
 
 
 if __name__ == "__main__":
 
+    Detect_face()
 
     cv.waitKey(0)
     cv.destroyAllWindows()
